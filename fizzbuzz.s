c714e915945ee8fcf7906b346158b5596e08837e
tomto464 <kajita.shun@b.mbox.nagoya-u.ac.jp>
2020-12-22 17:24:14 +0900

    １５の倍数に対してfizzbuzzを表示

--- fizzbuzz.s
+++ fizzbuzz.s
@@ -2,9 +2,22 @@
 .global main
 
 main:
-	mov rcx, 10
+	mov rcx, 16
 
 loop:
+	# count % 15
+	# 答え:rax　余り:rdx
+	# 割られる数: rdx or rax
+	# 割る数:  div <reg>
+	
+	mov rdx, 0
+	mov rax, rcx
+	mov r15, 15
+	div r15
+	cmp rdx, 0
+	jne next
+
+	# call write
 	mov rdx, 0x9
 	lea rsi, [fizzbuzz]
 	mov rdi, 0x1
@@ -12,6 +25,8 @@ loop:
 	push rcx
 	syscall
 	pop rcx
+
+next:
 	dec rcx	
 	je exit
 	jmp loop
