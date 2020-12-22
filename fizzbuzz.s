dd6e0ebda6bad0f1a4e82524e968a542a0e24558
tomto464 <kajita.shun@b.mbox.nagoya-u.ac.jp>
2020-12-22 18:18:03 +0900

    ０から始まり１６まででfizzbuzzを表示できる。

--- fizzbuzz.s
+++ fizzbuzz.s
@@ -2,24 +2,46 @@
 .global main
 
 main:
-	mov rcx, 16
+	mov rcx, 1
 
 loop:
-	# count % 15
+	# fizz
 	# 答え:rax　余り:rdx
 	# 割られる数: rdx or rax
 	# 割る数:  div <reg>
 	
 	mov rdx, 0
 	mov rax, rcx
-	mov r15, 15
+	mov r15, 3
+	div r15
+	cmp rdx, 0
+	jne buzznext
+
+	# call write
+	mov rdx, 0x4
+	lea rsi, [fizz]
+	mov rdi, 0x1
+	mov rax, 0x1
+	push rcx
+	syscall
+	pop rcx
+
+buzznext:
+	# buzz
+	# 答え:rax　余り:rdx
+	# 割られる数: rdx or rax
+	# 割る数:  div <reg>
+	
+	mov rdx, 0
+	mov rax, rcx
+	mov r15, 5
 	div r15
 	cmp rdx, 0
 	jne next
 
 	# call write
-	mov rdx, 0x9
-	lea rsi, [fizzbuzz]
+	mov rdx, 0x4
+	lea rsi, [buzz]
 	mov rdi, 0x1
 	mov rax, 0x1
 	push rcx
@@ -27,7 +49,17 @@ loop:
 	pop rcx
 
 next:
-	dec rcx	
+	# call write
+	mov rdx, 0x1
+	lea rsi, [newline]
+	mov rdi, 0x1
+	mov rax, 0x1
+	push rcx
+	syscall
+	pop rcx
+
+	inc rcx	
+	cmp rcx, 0x10
 	je exit
 	jmp loop
 
@@ -35,9 +67,9 @@ exit:
 	mov rax, 0x3c
 	syscall
 
-fizzbuzz:
-	.ascii "fizzbuzz\n"
 fizz:
-	.ascii "fizz\n"
+	.ascii "fizz"
 buzz:
-	.ascii "buzz\n"
+	.ascii "buzz"
+newline:
+	.ascii "\n"
